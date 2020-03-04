0a15bea657a0d67842345543d86e59b55dbf5466
vanill4Sky <wojtek-s22@wp.pl>
2020-03-04 20:35:35 +0100

    lab 2 zad 1 done

---
+++ 02/zad1.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash 
+set -e #exit on command fail
+set -u #exit on undefined variable use
+set -x #trace what gets executed
+
+if [ "${#}" -ne 1 ]; then
+	echo "Wrong number of arguments!"
+	exit 1
+fi
+
+if [ ! -d "${1}" ]; then
+	echo "Directory \"${1}\" does not exist!"
+	exit 1
+fi
+
+rm -f "${1}/"*.old
+for file_name in "${1}/"*; do
+	if [ -w "${file_name}" ]; then
+		mv "${file_name}" "${file_name}.old"
+	fi
+done
---
+++ 02/zad2.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash 
+set -e #exit on command fail
+set -u #exit on undefined variable use
+set -x #trace what gets executed
+
+if [ "${#}" -ne 2 ]; then
+	echo "Wrong number of arguments!"
+	exit 1
+elif [ ! -d "${1}" ]; then
+	echo "Directory \"${1}\" does not exist!"
+	exit 1
+elif [ ! -r "${2}" ]; then
+	echo "File \"${2}\" does not exist!"
+	exit 1
+fi
