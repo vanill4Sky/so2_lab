d7d018052348ac2a816cab998b014030032c04b6
vaniil4Sky <wojtek-s22@wp.pl>
2020-03-09 12:46:56 +0100

    lab 2 zad 11 - done

---
+++ 02/update_readme.py
@@ -0,0 +1,33 @@
+opening_token = "{{{"
+closing_token = "}}}"
+
+template_file = open("readme_template.md", "r")
+template_data =  template_file.read()
+
+output = ""
+
+start = 0
+end = template_data.rfind(closing_token) + len(closing_token)
+
+while(start < end):
+    opening_token_pos = template_data.find(opening_token, start, end)
+    output += template_data[start:opening_token_pos]
+    closing_token_pos = template_data.find(closing_token, opening_token_pos + len(opening_token), end)
+    start = closing_token_pos + len(closing_token)
+
+    script_filename = template_data[
+        opening_token_pos + len(opening_token) : closing_token_pos]
+    try:
+        script_file_data = open(script_filename, "r").read()
+        script_file_data = script_file_data.replace("\n", "\n\t")
+    except:
+        script_file_data = "in progress"
+    
+    output += script_file_data
+
+output += template_data[start:len(template_data)]
+
+output_file = open("readme.md", "w")
+output_file.write(output)
+
+# flake8
--- 02/zad11.sh
+++ 02/zad11.sh
@@ -5,22 +5,30 @@ set -u #exit on undefined variable use
 
 if [ "$#" -ne 2 ]; then
 	echo "Wrong number of arguments!"
-	echo "Usage: ${0} dir_a dir_b"
+	echo "Usage: ${0} directory list_file"
 	exit 1
 elif [ ! -d "${1}" ]; then
 	echo "Directory \"${1}\" does not exist!"
 	exit 1
-elif [ ! -d "${2}" ]; then
-	echo "Directory \"${2}\" does not exist!"
+elif [ ! -r "${2}" ]; then
+	echo "File \"${2}\" does not exist!"
 	exit 1
 fi
 
+list=`cat "${2}"`
+
+echo "Files not in the list:"
 for file in "${1}/"*; do
-	if [ ! -d "${file}" ]; then
-		filename=$(basename -- "${file}")
-		path_in_dir_b="${2}/${filename}"
-		if [ ! -d "${path_in_dir_b}" ] && [ -e "${path_in_dir_b}" ]; then
-			echo "${1}/${filename} --- ${path_in_dir_b}"
-		fi
+	filename=$(basename -- "${file}")
+
+	if [[ "${list}" != *"${filename}"* ]]; then
+		echo "${filename}"
+	fi
+done
+
+echo "Files not in the directory:"
+for filename in ${list}; do
+	if [[ ! -e "${1}/${filename}" ]]; then
+		echo "${filename}"
 	fi
 done
