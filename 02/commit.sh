e1dc91851742c68cc204c5371126d8ea106f3275
vaniil4Sky <wojtek-s22@wp.pl>
2020-03-09 15:01:12 +0100

    added helper script

---
+++ 02/commit.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash 
+set -e #exit on command fail
+set -u #exit on undefined variable use
+#set -x #trace what gets executed
+
+if [ "$#" -ne 1 ]; then
+	echo "Wrong number of arguments!"
+	echo "Usage: ${0} message"
+	exit 1
+fi
+
+python3 update_readme.py
+git add *
+git commit -m "${1}"
+git pull
